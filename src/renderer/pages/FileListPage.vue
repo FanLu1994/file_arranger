3be6d1973ff20601a91f55d7b3a2040e3745cbca
fan <1066580119@qq.com>
2021-03-22 22:01:33 +0800

    创建文件夹

--- src/renderer/pages/FileListPage.vue
+++ src/renderer/pages/FileListPage.vue
@@ -1,21 +1,21 @@
 <template>
 
-  <div>
+  <div class="file_list_wrapper">
 
     <Row :gutter="20" id="back_home_wrapper">
       <Col span="12" offset="2" >
 <!--        <Icon type="ios-arrow-back" @click="gotoMainPage" id="home_icon" size="30" color="#35AC5E"></Icon>-->
-        <Button  style="background-color:#35AC5E; color: white" icon="ios-arrow-back" @click="gotoMainPage">返回首页</Button>
+        <Button  style="background-color:#35AC5E; color: white;font-size: 20px" icon="ios-arrow-back" @click="gotoMainPage">返回首页</Button>
       </Col>
 
     </Row>
 
     <Row :gutter="20" id="file_wrapper">
-      <Col span="14" offset="2">
+      <Col span="16" offset="2">
         <div v-for="type in Object.keys(fileMap)" :id="'type-'+type">
-          <Button type="success" ghost> 类型:{{type}}</Button>
-          <Button type="success" ghost> 移动到新文件夹</Button>
-          <Button type="success" ghost> 复制到新文件夹</Button>
+          <Button type="success" ghost class="btn"> 类型:{{type}}</Button>
+          <Button type="success" ghost class="btn" @click="openDir(type)"> 移动到新文件夹</Button>
+          <Button type="success" ghost class="btn" @click="openDir(type)"> 复制到新文件夹</Button>
 
           <List   border id="list">
             <ListItem v-for="file in fileMap[type]">{{ getFileName(file) }}</ListItem>
@@ -24,7 +24,7 @@
         </div>
       </Col>
 
-      <Col span="6" offset="2">
+      <Col span="5" offset="1">
         <Anchor show-ink  >
           <AnchorLink v-for="type in Object.keys(fileMap)" :href="'#type-'+type" :title="'类型:'+type" />
         </Anchor>
@@ -43,12 +43,16 @@
 import Classifier from '../utils/FileClassfication'
 import { mapGetters } from 'vuex'
 const path = require("path");
+const fs = require("fs");
+const { remote } = require('electron');
 
 export default {
 name: "FileListPage",
   data(){
     return {
-      fileList: [1,2,3,4,5,6,7,8,9]
+      fileList: [1,2,3,4,5,6,7,8,9],
+
+      targetPath:"",
     }
   },
 
@@ -73,7 +77,39 @@ name: "FileListPage",
     getFileName(filePath){
       let fileInfo =  path.parse(filePath)
       return fileInfo.name+fileInfo.ext
-    }
+    },
+
+    // 打开文件夹
+    openDir(type){
+      console.log(type)
+
+      // 选择文件夹
+      const result = remote.dialog.showOpenDialog({
+        properties: ['openFile',"openDirectory"],
+      });
+
+      if(result===undefined){
+        return
+      }
+
+      // 新建文件夹
+      console.log(result[0])
+      this.targetPath = path.join(result[0],"类型"+type)
+      console.log(this.targetPath)
+
+
+      let that = this
+      fs.access(that.targetPath,function (err){
+        if(err){//如果文件不存在，就创建这个文件
+          fs.mkdir(that.targetPath,function (err) {
+            console.log("创建文件夹成功")
+          });
+        }else{
+          //如果这个文件已经存在，就进入下一个循环
+          console.log("文件夹已经存在")
+        }
+      })
+    },
 
   }
 }
@@ -83,6 +119,10 @@ name: "FileListPage",
 #box{
   margin-left: 15%;
   margin-right: 15%;
+}
+
+.file_list_wrapper{
+  font-size: 18px;
 }
 
 #box ul{
@@ -123,5 +163,10 @@ name: "FileListPage",
   margin-top: 10px;
   margin-bottom: 20px;
 }
+
+.btn{
+  font-size: 18px;
+}
+
 
 </style>
